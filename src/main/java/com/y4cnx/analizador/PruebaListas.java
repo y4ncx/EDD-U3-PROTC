0cd53ddd3bf0917f3c1362f5b8cc0b78a527be33
y4ncx <y4ncxs@gmail.com>
2025-11-06 19:37:36 -0500

    se agrega la clase PruebaLista y se le colocan los numeros a numeros.txt

---
+++ src/main/java/com/y4cnx/analizador/PruebaListas.java
@@ -0,0 +1,26 @@
+package com.y4cnx.analizador;
+
+import com.y4ncx.analizador.estructuras.*;
+
+public class PruebaListas {
+    public static void main(String[] args) {
+        System.out.println("=== Lista Simple ===");
+        SinglyLinkedList<Integer> simple = new SinglyLinkedList<>();
+        simple.addLast(10);
+        simple.addLast(20);
+        simple.addFirst(5);
+        simple.printList();
+        simple.removeLast();
+        simple.printList();
+
+        System.out.println("\n=== Lista Doble ===");
+        DoublyLinkedList<Integer> doble = new DoublyLinkedList<>();
+        doble.addFirst(1);
+        doble.addLast(2);
+        doble.addLast(3);
+        doble.printForward();
+        doble.printBackward();
+        doble.removeLast();
+        doble.printForward();
+    }
+}
